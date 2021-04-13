57c60ca624cbdf1d3b84377d8a2388c5a912d217
sophia wheatley <29500039+sophiawheatley@users.noreply.github.com>
2021-04-13 23:07:23 +0200

    Update clustering_blog.R

--- clustering_blog.R
+++ clustering_blog.R
@@ -1,62 +1,91 @@
-clust_data <- data.frame(X= runif(10, 0, 1), Y= runif(10, 0, 1), n=c(1:10))
+rm(list=ls())
 
-library(ggplot2)
+pacman::p_load(ggplot2, dplyr, fpc)
+
+# clust_data <- data.frame(X= runif(10, 0, 1), Y= runif(10, 0, 1))
+# save.image(file = "clustering_blog.RData")
+load("clustering_blog.RData")
+
 #palette posted on https://stackoverflow.com/questions/57153428/r-plot-color-combinations-that-are-colorblind-accessible
 color_blind_palette <- c("#88CCEE", "#CC6677", "#DDCC77", "#117733", "#332288", "#AA4499", 
                                                     "#44AA99", "#999933", "#882255", "#661100", "#6699CC", "#888888")
 
-ggplot(clust_data, aes(x=X, y= Y,color=as.factor(n)))+
+clust_data_gg <- clust_data %>% mutate(n = c(1:10))
+ggplot(clust_data_gg, aes(x=X, y= Y,color=as.factor(n)))+
   geom_point(show.legend = FALSE)+
   theme_bw()+
-  geom_text(aes(X,Y,label=n), show.legend = F, position = position_nudge(x = 0.025))+
-  scale_fill_manual(values=color_blind_palette[1:nrow(clust_data)])
+  geom_text(aes(X,Y,label=n), show.legend = F, position = position_nudge(x = 0.03))+
+  scale_fill_manual(values=color_blind_palette[1:nrow(clust_data_gg)])+
+  coord_fixed()
 
 #calculate distance matrix
 #manual way
 
 dist_matrix = matrix(nrow=10, ncol=10)
-clust_data_loop <- clust_data[,-3]
 for (i in 1:10){
   for (j in 1:10){
-    dist_val <- sqrt((clust_data$X[i] - clust_data$X[j])**2 + (clust_data$Y[i] - clust_data$Y[j])**2)
-    print(dist_val)
-    dist_matrix[i,j] <- dist_val
+    if (i > j){
+      dist_val <- sqrt((clust_data$X[i] - clust_data$X[j])**2 + (clust_data$Y[i] - clust_data$Y[j])**2)
+      dist_matrix[i,j] <- dist_val
+    }
   }
 }
 
 #dist() function
-dist_matrix_2 <- matrix(dist(clust_data[,-3], method="euclidean",diag=F))
+dist_matrix_2 <- dist(clust_data[,-3], method="euclidean",diag=F)
 
 #clustering using average linkage
-#find smallest distance value 
-min(dist_matrix[dist_matrix > 0])
 
-#get index 
-which(dist_matrix==min(dist_matrix[dist_matrix > 0]), arr.ind=TRUE)
-
-#update distance matrix with average linkage
-#dist((P5,P6),P1) 
-0.5*((dist_matrix[5,1])+(dist_matrix[6,1]))
-
-#merge cluster function 
-merge_cluster <- function(mat, datapoint) {
-  min_dist <- min(mat[mat > 0])
-  index <- which(mat==min_dist, arr.ind=TRUE)[1,]
-  val <- mat[index[["row"]], index[["col"]]]
-  new_val <- 0.5*((mat[index[["row"]],datapoint]) + (mat[index[["col"]],datapoint])) 
-  cluster_name <- paste0(paste0(index[["row"]], "_", index[["col"]],"_cluster"))
+#average linkage  
+average_linkage <- function(mat, datapoint) {
+  min_dist <- min(mat, na.rm = TRUE) #find smallest distance value 
+  min_index <- which(mat==min_dist, arr.ind=TRUE)[1,] #get row and col indices
+  min_row <- index[["row"]]
+  min_col <- index[["col"]]
+  comp_pair_1 <- if(is.na((mat[min_row,datapoint]))) {(mat[datapoint, min_row])} else {mat[min_row,datapoint]}
+  comp_pair_2 <- if(is.na((mat[min_col,datapoint]))) {(mat[datapoint, min_col])} else {mat[min_col,datapoint]}
+  new_val <- (comp_pair_1  + comp_pair_2) / 2 #calculate average linkage
+  cluster_name <- c(paste0(min_row, "_", min_col))
+  return(c(cluster_name, new_val))
 }
 
-
-updated_matrix <- dist_matrix
+updated_matrix <- data.frame()
 for (i in 1:10){
-    cluster <- merge_cluster(updated_matrix, i)
-    print(cluster)
+      cluster <- average_linkage(dist_matrix, i)
+      updated_matrix <- rbind(updated_matrix, cluster)
 }
+updated_matrix <- na.omit(updated_matrix)
 
 ##hclust()
 tree <- hclust(as.dist(dist_matrix), method = "average")
 dend_plot <- as.dendrogram(tree)
 plot(dend_plot)
-rect.hclust(tree, k = 3, border=color_blind_palette[1:3]) # draw rectangles
+rect.hclust(tree, k = 2, border=color_blind_palette[1:2]) # draw rectangles
 
+# explain how hclust works 
+tree$height #the distance values in increasing order, with the minimum value first being the first cluster
+tree$merge #the clusters starting with those closest together by means of average linkage and furthest way
+# positive values indicate the clusters and negative values the data points themselves 
+# we can compare the simple scatterplot with our dendrogram and see the correspondence between the data points and our clusters
+# points 5 & 8 which are close together form a cluster
+
+
+#choose number of clusters, bear in mind this is a small dataset with random samples
+ch_index_list <- c()
+dunn_index_list <- c()
+for (n_clust in 2:10){
+  tree_cut<-cutree(hclust(as.dist(dist_matrix), method="average"), k=n_clust)
+  cluster_stats<-cluster.stats(d=as.dist(dist_matrix), tree_cut, silhouette=T, sepindex=T)
+  ch_index_list[n_clust] <- cluster_stats$ch
+  dunn_index_list[n_clust] <- cluster_stats$dunn2
+}
+
+plot(ch_index_list, xlab="k (number of clusters)", ylab = "Calinski and Harabasz Index")
+max(ch_index_list,na.rm = T)
+plot(dunn_index_list, xlab="k (number of clusters)", ylab = "Dunn Index")
+
+#choose 'best' number of clusters and cut tree 
+k_value=2
+cutree(tree, k=k_value)
+plot(tree, cex=0.6) # plot tree
+rect.hclust(tree, k=k_value, border=color_blind_palette[1:k_value])
